15acfdd8f0aa1b4ff292364f8352fc609bb70906
Daniel-Sogbey <mathematics06physics@gmail.com>
2021-04-15 23:34:43 -0700

    new setup

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
-const express = require("express");
+// const express = require("express");
 const { Client } = require("@googlemaps/google-maps-services-js");
 const dotenv = require("dotenv");
 dotenv.config();
-
-const app = express();
-
-app.use(express.json());
 
 const client = new Client({});
 
 console.log(process.env.GOOGLE_API_KEY);
 
-app.post("/dir", async (req, res) => {
-	const { lat, lng } = req.body;
-	console.log(req.body, lat, lng);
-	try {
-		const result = `https://maps.googleapis.com/maps/api/directions/json?origin=${(
-			lat,
-			lng
-		)}
-		&key=${process.env.GOOGLE_API_KEY}`;
-		res.status(200).send(result.data);
-	} catch (error) {
-		res.status(403).send(error, "not working");
-	}
-});
-
-app.listen(3000, () => console.log("server running on port 3000"));
+client
+	.elevation({
+		params: {
+			locations: [{ lat: 45, lng: -110 }],
+			key: process.env.GOOGLE_API_KEY,
+		},
+		timeout: 1000, // milliseconds
+	})
+	.then(error => {
+		console.log(error);
+	})
+	.catch(e => {
+		console.log(e.response.data.error_message);
+	});
