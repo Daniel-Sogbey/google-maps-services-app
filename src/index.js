20f68f44fe3f8505cf139e890a6481fc2532fdf1
Daniel-Sogbey <mathematics06physics@gmail.com>
2021-04-15 23:45:02 -0700

    working with places api

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-// const express = require("express");
-const { Client } = require("@googlemaps/google-maps-services-js");
+const express = require("express");
+const axios = require("axios");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const client = new Client({});
+// const { Client } = require("@googlemaps/google-maps-services-js");
 
-console.log(process.env.GOOGLE_API_KEY);
+const app = express();
 
-client
-	.elevation({
-		params: {
-			locations: [{ lat: 45, lng: -110 }],
-			key: process.env.GOOGLE_API_KEY,
-		},
-		timeout: 1000, // milliseconds
-	})
-	.then(error => {
-		console.log(error);
-	})
-	.catch(e => {
-		console.log(e.response.data.error_message);
-	});
+const key = process.env.GOOGLE_API_KEY;
+
+app.get("/", async (req, res, next) => {
+	try {
+		const neighborhood = "chelsea";
+		const borough = "manhattan";
+		const city = "new+york+city";
+		const category = "burgers";
+		const { data } = await axios.get(
+			`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${category}+${neighborhood}+${borough}+${city}&type=restaurant&key=${key}`
+		);
+		console.log(data);
+		res.send(data);
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+app.listen(3000, () => console.log("Server on"));
